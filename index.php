dd5b22cf1259ad6999e41a945c5a0ace1434d306
sebbekarlsson <sebbekarlsson97@gmail.com>
2015-01-13 21:13:06 -0800

    initial commit

---
+++ index.php
@@ -0,0 +1,48 @@
+<html>
+
+	<header>
+		<link rel="stylesheet" type="text/css" href="styles/style.css">
+		<link href='http://fonts.googleapis.com/css?family=Roboto' rel='stylesheet' type='text/css'>
+		<link href='http://fonts.googleapis.com/css?family=Quicksand' rel='stylesheet' type='text/css'>
+		<link href='http://fonts.googleapis.com/css?family=Cinzel+Decorative' rel='stylesheet' type='text/css'>
+		<meta charset="UTF-8">
+	</header>
+
+	<body>
+		<div class="container">
+			<div class="topbar"><div>Classic Yacht Service</div></div>
+			<div class="heroholder" style="background-image:url(images/yacht.jpg);">
+				<div class="hero">
+				</div>
+			</div>
+			<div class="topbar"><div></div></div>
+
+			<div class="sidebar">
+				<a class="sidebtn">Hem</a>
+				<a class="sidebtn">Om CYS</a>
+				<a class="sidebtn">Tjänster</a>
+				<a class="sidebtn">WHITE 23</a>
+				<a class="sidebtn">XtreemCoat</a>
+				<a class="sidebtn">MAS Epoxies</a>
+				<a class="sidebtn">KiwiGrip</a>
+				<a class="sidebtn">Pågående Arbeten</a>
+				<a class="sidebtn">Tidigare Arbeten</a>
+				<a class="sidebtn">Till salu</a>
+				<a class="sidebtn">Evenemang</a>
+				<a class="sidebtn">Kontakta CYS</a>
+			</div>
+			<div class="content">
+				<div class="text">
+					<h1>Livskvalitet är en vacker träbåt</h1>
+					Hur mår din träbåt? Kanske behöver något bytas eller bara fräschas upp? Kanske finns en dröm om en nybyggd båt vid bryggan? Vi erbjuder lösningar på era funderingar och önskemål. Vad ni än behöver ha gjort så gör vi noggranna arbeten med hög kvalitet. Både traditionella och moderna metoder används för bästa resultat. Classic Yacht Service erbjuder också hållbara och underhållsfria produkter för Din båt. 
+Välkommen till Classic Yacht Service!
+				</div>
+			</div>
+
+		</div>
+	</body>
+
+	<footer>
+	</footer>
+
+</html>
