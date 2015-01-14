73b8c0300ab4a6eb9a743c43e40d09a201834732
sebbekarlsson <sebbekarlsson97@gmail.com>
2015-01-13 21:34:38 -0800

    initial update

---
+++ apps/main.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+	$(".under").hide();
+	$(".sidebtn").click(function(){
+		$(".sidebtn").each(function(){
+			$(this).find(".under").fadeOut();
+		});
+		$(this).find(".under").fadeIn();
+		if($(this).find(".under > .subcat").length < 3){
+			$(this).find(".under").append("<a class='subcat'>Underkategori</a>");
+			$(this).find(".under").append("<a class='subcat'>Underkategori</a>");
+			$(this).find(".under").append("<a class='subcat'>Underkategori</a>");
+		}
+	});
+});
--- index.php
+++ index.php
@@ -5,6 +5,8 @@
 		<link href='http://fonts.googleapis.com/css?family=Roboto' rel='stylesheet' type='text/css'>
 		<link href='http://fonts.googleapis.com/css?family=Quicksand' rel='stylesheet' type='text/css'>
 		<link href='http://fonts.googleapis.com/css?family=Cinzel+Decorative' rel='stylesheet' type='text/css'>
+		<script src="apps/jquery.js"></script>
+		<script src="apps/main.js"></script>
 		<meta charset="UTF-8">
 	</header>
 
@@ -18,24 +20,24 @@
 			<div class="topbar"><div></div></div>
 
 			<div class="sidebar">
-				<a class="sidebtn">Hem</a>
-				<a class="sidebtn">Om CYS</a>
-				<a class="sidebtn">Tjänster</a>
-				<a class="sidebtn">WHITE 23</a>
-				<a class="sidebtn">XtreemCoat</a>
-				<a class="sidebtn">MAS Epoxies</a>
-				<a class="sidebtn">KiwiGrip</a>
-				<a class="sidebtn">Pågående Arbeten</a>
-				<a class="sidebtn">Tidigare Arbeten</a>
-				<a class="sidebtn">Till salu</a>
-				<a class="sidebtn">Evenemang</a>
-				<a class="sidebtn">Kontakta CYS</a>
+				<a class="sidebtn">Hem<div class="under"></div></a>
+				<a class="sidebtn">Om CYS<div class="under"></div></a>
+				<a class="sidebtn">Tjänster<div class="under"></div></a>
+				<a class="sidebtn">WHITE 23<div class="under"></div></a>
+				<a class="sidebtn">XtreemCoat<div class="under"></div></a>
+				<a class="sidebtn">MAS Epoxies<div class="under"></div></a>
+				<a class="sidebtn">KiwiGrip<div class="under"></div></a>
+				<a class="sidebtn">Pågående Arbeten<div class="under"></div></a>
+				<a class="sidebtn">Tidigare Arbeten<div class="under"></div></a>
+				<a class="sidebtn">Till salu<div class="under"></div></a>
+				<a class="sidebtn">Evenemang<div class="under"></div></a>
+				<a class="sidebtn">Kontakta CYS<div class="under"></div></a>
 			</div>
 			<div class="content">
 				<div class="text">
 					<h1>Livskvalitet är en vacker träbåt</h1>
 					Hur mår din träbåt? Kanske behöver något bytas eller bara fräschas upp? Kanske finns en dröm om en nybyggd båt vid bryggan? Vi erbjuder lösningar på era funderingar och önskemål. Vad ni än behöver ha gjort så gör vi noggranna arbeten med hög kvalitet. Både traditionella och moderna metoder används för bästa resultat. Classic Yacht Service erbjuder också hållbara och underhållsfria produkter för Din båt. 
-Välkommen till Classic Yacht Service!
+					Välkommen till Classic Yacht Service!
 				</div>
 			</div>
 
